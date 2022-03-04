f6eb7cf4aa9aebb2dc7508bf862816d71f87cae9
alexgarcia-dev <alexgarcia_rox@hotmail.com>
2022-03-03 21:38:37 -0300

    1.1

--- index.py
+++ index.py
@@ -20,8 +20,15 @@ for i, mensagem in enumerate(contatos_df['Mensagem']):
     texto = urllib.parse.quote(f"Bom dia, {pessoa}! {mensagem}")
     link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
     navegador.get(link)
-    while len(navegador.find_elements_by_id("side")) < 1:
-        time.sleep(1)
-    time.sleep(5)
-    navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button').send_keys(Keys.ENTER)
-    time.sleep(10)
+
+    time.sleep(3)
+
+    if navegador.find_element_by_xpath('//*[@id="app"]/div[1]/span[2]/div[1]/span/div[1]/div/div/div/div/div[1]'):
+        time.sleep(5)
+        navegador.find_element_by_xpath('//*[@id="app"]/div[1]/span[2]/div[1]/span/div[1]/div/div/div/div/div[2]/div').send_keys(Keys.ENTER)
+    else:
+        while len(navegador.find_elements_by_id("side")) < 1:
+            time.sleep(1)
+        time.sleep(5)
+        navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button').send_keys(Keys.ENTER)
+        time.sleep(10)
